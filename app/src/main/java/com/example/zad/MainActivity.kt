0bfa3d25c11adc28edce1be0cc6a1c19c5532ee7
student <tysia_ania@wp.pl>
2021-09-10 15:40:09 +0200

    mainActivity

--- app/src/main/java/com/example/zad/MainActivity.kt
+++ app/src/main/java/com/example/zad/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.zad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val jeden =  findViewById<Button>(R.id.jeden)
+        val dwa =  findViewById<Button>(R.id.dwa)
+        val trzy =  findViewById<Button>(R.id.trzy)
+        val cztery =  findViewById<Button>(R.id.cztery)
+        val piec =  findViewById<Button>(R.id.piec)
+        val szesc =  findViewById<Button>(R.id.szesc)
+        val siedem =  findViewById<Button>(R.id.siedem)
+        val osiem =  findViewById<Button>(R.id.osiem)
+        val dziewiec =  findViewById<Button>(R.id.dziewiec)
+        val plus =  findViewById<Button>(R.id.plus)
+        val minus =  findViewById<Button>(R.id.minus)
+        val razy =  findViewById<Button>(R.id.razy)
+        val podzielic =  findViewById<Button>(R.id.podzielic)
+        val rownasie =  findViewById<Button>(R.id.rownasie)
+        val wynik =  findViewById<TextView>(R.id.wynik)
+        val dzialania =  findViewById<TextView>(R.id.dzialania)
     }
 }
